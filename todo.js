8937c65a472973adae012598fbf75a516f067bb4
salma <salma.hossam.harb@gmail.com>
2023-12-17 13:32:01 +0200

    i addded the accordian

--- todo.js
+++ todo.js
@@ -3,19 +3,29 @@ function hide(divId) {
     document.getElementById(divId).classList.add("invisible");
     confirm("undo");
     if (confirm("undo") == true) {
-        text = "You pressed undo!";
+        document.getElementById(divId).classList.remove("invisible");
       } else {
         text = "You canceled!";
-        document.getElementById(divId).classList.remove("invisible");
+        text = "You pressed undo!";
       }
-      
+    }
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
     
-}
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      }
+);
 
-
-
-
-
+    }
 
 
 
