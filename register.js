ca111b83d89bf1c5be07648dc5687e5ee3edec3f
fatma <fatmaashraf433@gmail.com>
2023-12-22 22:42:48 +0200

    Registration"

---
+++ register.js
@@ -0,0 +1,7 @@
+document.getElementById("submitbutton").addEventListener("click",handleSubmitClick);
+
+function handleSubmitClick(){
+     console.log("here");
+     window.location.href="\login.html";
+}
+
