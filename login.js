0ba0ad703a6936b98e2043544fb65cf6b2c79ae7
salma <salma.hossam.harb@gmail.com>
2023-12-29 19:30:18 +0200

    Add token to local storagey and removed the validate and submitpassword functions

--- login.js
+++ login.js
@@ -1,60 +1,5 @@
 
-function validatePassword() {
 
-    var pass = document.getElementById("pass").value;
-    var numbers = document.getElementById("numbers");
-    var lowercase = document.getElementById("lowercase");
-    var uppercase = document.getElementById("uppercase");
-    var specialchars = document.getElementById("specialchars");
-    var charLen8 = document.getElementById("charLen8");
-
-    var specialcharsList = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-    var lowercaseList = /[a-z]/;
-    var uppercaseList = /[A-Z]/;
-    var numbersList = /[0-9]/;
-
-    if (pass.match(numbersList)) {
-        numbers.classList.remove("invalid");
-        numbers.classList.add("valid");
-    }
-    else {
-        numbers.classList.remove("valid");
-        numbers.classList.add("invalid");
-    }
-    if (pass.match(lowercaseList)) {
-        lowercase.classList.remove("invalid");
-        lowercase.classList.add("valid");
-    }
-    else {
-        lowercase.classList.remove("valid");
-        lowercase.classList.add("invalid");
-    }
-    if (pass.match(uppercaseList)) {
-        uppercase.classList.remove("invalid");
-        uppercase.classList.add("valid");
-    }
-    else {
-        uppercase.classList.remove("valid");
-        uppercase.classList.add("invalid");
-    }
-    if (pass.match(specialcharsList)) {
-        specialchars.classList.remove("invalid");
-        specialchars.classList.add("valid");
-    }
-    else {
-        specialchars.classList.remove("valid");
-        specialchars.classList.add("invalid");
-    }
-    if (pass.length >= 8) {
-        charLen8.classList.remove("invalid");
-        charLen8.classList.add("valid");
-    }
-    else {
-        charLen8.classList.remove("valid");
-        charLen8.classList.add("invalid");
-    }
-
-}
 
 function toggleIcon() {
     var pass = document.getElementById("pass");
@@ -71,29 +16,6 @@ function toggleIcon() {
     }
 }
 
-function submitPassword() {
-    var numbers = document.getElementById("numbers");
-    var lowercase = document.getElementById("lowercase");
-    var uppercase = document.getElementById("uppercase");
-    var specialchars = document.getElementById("specialchars");
-    var charLen8 = document.getElementById("charLen8");
-
-    var array = [numbers, lowercase, uppercase, specialchars, charLen8];
-    var classLists = [];
-    var flag = 1;
-    for (var i = 0; i < 5; i++) {
-        classLists[i] = array[i].className;
-        if (classLists[i] == "invalid") {
-            window.alert("Password is invalid!");
-            flag = 0;
-            break;
-        }
-    }
-
-    if (flag == 1) {
-        window.alert("Password is valid!");
-    }
-}
 
 document.getElementById("submitbutton").addEventListener("click", handleSubmitClick);
 
@@ -107,29 +29,24 @@ async function handleSubmitClick() {
             },
             body: JSON.stringify({
                 username: document.getElementById('user').value,
-                password:  document.getElementById('pass').value
+                password: document.getElementById('pass').value
             })
         });
         if (response.status === 200) {
-            let resp = await response.json()
-            // TODO: Add token to local storagey
-             window.location.href="\calendar.html";
+            let resp = await response.json();
+            localStorage.setItem('tokenKey', resp.token);
+            window.location.href = "\calendar.html";
         }
     }
 }
 
-function validateInputs(){
+function validateInputs() {
     let username = document.getElementById('user').value
     let password = document.getElementById('pass').value
-    if(username.trim() !== "" && password.trim() !==""){
-       if(validatePassword()){
-        return true
-       }
-       return false;
+    if (username.trim() !== "" && password.trim() !== "") {
+       return true
+    
     }
     return false;
 }
 
-function validatePassword() {
-    return true;
-}
