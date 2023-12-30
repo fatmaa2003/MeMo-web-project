3b7e6be1c63c3d1bcab4bc15de94f01dbd27823a
salma <salma.hossam.harb@gmail.com>
2023-12-30 17:58:32 +0200

    the password validation

--- register.js
+++ register.js
@@ -1,61 +1,107 @@
-function validatePassword() {
+function validatePassword(){
 
-     var pass = document.getElementById("pass").value;
-     var numbers = document.getElementById("numbers");
-     var lowercase = document.getElementById("lowercase");
-     var uppercase = document.getElementById("uppercase");
-     var specialchars = document.getElementById("specialchars");
-     var charLen8 = document.getElementById("charLen8");
- 
-     var specialcharsList = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-     var lowercaseList = /[a-z]/;
-     var uppercaseList = /[A-Z]/;
-     var numbersList = /[0-9]/;
- 
-     if (pass.match(numbersList)) {
-         numbers.classList.remove("invalid");
-         numbers.classList.add("valid");
-     }
-     else {
-         numbers.classList.remove("valid");
-         numbers.classList.add("invalid");
-     }
-     if (pass.match(lowercaseList)) {
-         lowercase.classList.remove("invalid");
-         lowercase.classList.add("valid");
-     }
-     else {
-         lowercase.classList.remove("valid");
-         lowercase.classList.add("invalid");
-     }
-     if (pass.match(uppercaseList)) {
-         uppercase.classList.remove("invalid");
-         uppercase.classList.add("valid");
-     }
-     else {
-         uppercase.classList.remove("valid");
-         uppercase.classList.add("invalid");
-     }
-     if (pass.match(specialcharsList)) {
-         specialchars.classList.remove("invalid");
-         specialchars.classList.add("valid");
-     }
-     else {
-         specialchars.classList.remove("valid");
-         specialchars.classList.add("invalid");
-     }
-     if (pass.length >= 8) {
-         charLen8.classList.remove("invalid");
-         charLen8.classList.add("valid");
-     }
-     else {
-         charLen8.classList.remove("valid");
-         charLen8.classList.add("invalid");
-     }
- 
- }
+    var pass = document.getElementById("pass").value;
+    var numbers = document.getElementById("numbers");
+    var lowercase = document.getElementById("lowercase");
+    var uppercase = document.getElementById("uppercase");
+    var specialchars = document.getElementById("specialchars");
+    var charLen8 = document.getElementById("charLen8");
 
-document.getElementById("submitbutton").addEventListener("click", handleSubmitClick);
+    var specialcharsList = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    var lowercaseList = /[a-z]/;
+    var uppercaseList = /[A-Z]/;
+    var numbersList = /[0-9]/;
+
+    if(pass.match(numbersList)){
+        numbers.classList.remove("invalid");
+        numbers.classList.add("valid");
+    }
+    else{
+        numbers.classList.remove("valid");
+        numbers.classList.add("invalid");
+    }
+    if(pass.match(lowercaseList)){
+        lowercase.classList.remove("invalid");
+        lowercase.classList.add("valid");
+    }
+    else{
+        lowercase.classList.remove("valid");
+        lowercase.classList.add("invalid");
+    }
+    if(pass.match(uppercaseList)){
+        uppercase.classList.remove("invalid");
+        uppercase.classList.add("valid");
+    }
+    else{
+        uppercase.classList.remove("valid");
+        uppercase.classList.add("invalid");
+    }
+    if(pass.match(specialcharsList)){
+        specialchars.classList.remove("invalid");
+        specialchars.classList.add("valid");
+    }
+    else{
+        specialchars.classList.remove("valid");
+        specialchars.classList.add("invalid");
+    }
+    if (pass.length >= 8){
+        charLen8.classList.remove("invalid");
+        charLen8.classList.add("valid");
+    }
+    else {
+        charLen8.classList.remove("valid");
+        charLen8.classList.add("invalid");
+    }
+                                    
+}
+
+function toggleIcon(){
+    var pass = document.getElementById("pass");
+    var icon = document.getElementById("icon");
+    icon.classList.toggle("fa-eye-slash");
+    icon.classList.toggle("fa-eye");
+    var className = icon.className;
+    console.log(className);
+    if(className.match("fa-eye"))
+    {
+        pass.type = "password"; 
+    }
+    if(className.match("fa-eye-slash"))
+    {
+        pass.type = "text";
+    }
+}
+
+function submitPassword(){
+    var numbers = document.getElementById("numbers");
+    var lowercase = document.getElementById("lowercase");
+    var uppercase = document.getElementById("uppercase");
+    var specialchars = document.getElementById("specialchars");
+    var charLen8 = document.getElementById("charLen8");
+
+    var array = [numbers,lowercase,uppercase,specialchars,charLen8];
+    var classLists = [];
+    var flag = 1;
+    for(var i = 0;i<5;i++)
+    {
+        classLists[i] = array[i].className;
+        if(classLists[i] == "invalid")
+        {
+            window.alert("Password is invalid!");
+            flag = 0;
+            break;
+        }
+    } 
+    
+    if(flag == 1)
+    {
+        window.alert("Password is valid!");
+    }
+}
+
+document.getElementById("submitbutton").addEventListener("click",handleSubmitClick);
+
+
 
 
 async function handleSubmitClick() {
