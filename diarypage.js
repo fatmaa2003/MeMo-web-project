7a5dde303200813c48bc028e037fe515473f4680
salma <salma.hossam.harb@gmail.com>
2024-01-03 20:40:10 +0200

    last changes

--- diarypage.js
+++ diarypage.js
@@ -29,15 +29,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     let sidebar = document.querySelector(".sidebar");
     let closeBtn = document.querySelector("#btn");
-    let searchBtn = document.querySelector(".bx-search");
+   
     closeBtn.addEventListener("click", () => {
         sidebar.classList.toggle("open");
         menuBtnChange();//calling the function(optional)
     });
-    searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
-        sidebar.classList.toggle("open");
-        menuBtnChange(); //calling the function(optional)
-    });
+   
     // following are the code to change sidebar button(optional)
     function menuBtnChange() {
         if (sidebar.classList.contains("open")) {
